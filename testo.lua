dbb061e581e7472e7254d943c7266aadb13b6233
OrionLibrary <79996531+OrionLibrary@users.noreply.github.com>
2021-06-14 16:57:28 +0200

    Create testo.lua

---
+++ testo.lua
@@ -0,0 +1,33 @@
+local AFS = {}
+local RemoteFolder = game:GetService("ReplicatedStorage").Remote
+local PetFolder = game:GetService("Players").LocalPlayer.PlayerGui.MainGui.Pets.Main.Scroll
+local EggArguments = {
+    [1] = workspace.Worlds.OnePiece.OnePieceEgg,
+    [2] = 1
+}
+
+function AFS:AutoSell(pet1, pet2, pet3)
+    for i,v in pairs(PetFolder:GetChildren()) do
+        if v:IsA("ImageButton") then
+            if v.PetName.Text == pet1 or v.PetName.Text == pet2 or v.PetName.Text == pet3 then
+                local args2 = {
+                    [1] = v.UID.Value,
+                    [2] = "MakeGold"
+                }
+                local args = {
+                    [1] = v.UID.Value,
+                    [2] = "Sell"
+                }
+                
+                game:GetService("ReplicatedStorage").Remote.ManagePet:FireServer(unpack(args2))
+                game:GetService("ReplicatedStorage").Remote.ManagePet:FireServer(unpack(args))
+            end
+        end
+    end
+end
+
+function AFS:AutoBuy()
+    RemoteFolder.OpenEgg:InvokeServer(unpack(EggArguments))
+end
+
+return AFS
